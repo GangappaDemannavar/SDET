5875fa7cbca2c6b45eae4edfe7f50690db248675
chiragkumar <chiragkumar@DESKTOP-A62HQ47>
2021-12-10 12:59:07 +0530

    new

---
+++ src/test/java/Sdet/demo2Test.java
@@ -0,0 +1,15 @@
+package Sdet;
+
+import org.junit.Test;
+
+public class demo2Test {
+	@Test
+	public void add()
+	{
+		System.out.println("Addition of two number");
+		
+		System.out.println("substraction of two number");
+		System.out.println("substraction of two number");
+	}
+
+}
